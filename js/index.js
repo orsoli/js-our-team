da771a62268923416c052bbe3c88d2a4b282a655
orsoli <fljcoli@gmail.com>
2024-08-28 15:20:49 +0200

    Added Milestone 0

--- js/index.js
+++ js/index.js
@@ -1,1 +1,43 @@
+// --- Preparation phase
+// Create array object called data
+const profilesData = [
+  {
+    firstName: "Wayne",
+    lastName: "Barnett",
+    job: "Founder & CEO",
+    profileImg: "./img/wayne-barnett-founder-ceo.jpg",
+  },
+  {
+    firstName: "Angela",
+    lastName: "Caroll",
+    job: "Chief Editor ",
+    profileImg: "./img/angela-caroll-chief-editor.jpg",
+  },
+  {
+    firstName: "Walter",
+    lastName: "Gordon",
+    job: "Office Manager ",
+    profileImg: "./img/walter-gordon-office-manager.jpg",
+  },
+  {
+    firstName: "Angela",
+    lastName: "Lopez",
+    job: "Social Media Manager",
+    profileImg: "./img/angela-lopez-social-media-manager.jpg",
+  },
+  {
+    firstName: "Scott",
+    lastName: "Estrada",
+    job: "Developer",
+    profileImg: "./img/scott-estrada-developer.jpg",
+  },
+  {
+    firstName: "Barbara",
+    lastName: "Ramos",
+    job: "Graphic Designer",
+    profileImg: "./img/barbara-ramos-graphic-designer.jpg",
+  },
+];
 
+console.table(profilesData); // Testing Print in console.
+console.log(profilesData[2].firstName); // Testing Print in console.
---
+++ js/structure.js
@@ -0,0 +1,15 @@
+/* Project
+    Using the data below, create an array of objects to represent the team members.
+    Each member is characterized by name, role, and photo.
+
+   Data
+    Wayne Barnett |	Founder & CEO        |	wayne-barnett-founder-ceo.jpg
+    Angela Caroll |	Chief Editor         |	angela-caroll-chief-editor.jpg
+    Walter Gordon |	Office Manager       |	walter-gordon-office-manager.jpg
+    Angela Lopez  |	Social Media Manager |	angela-lopez-social-media-manager.jpg
+    Scott Estrada |	Developer            |	scott-estrada-developer.jpg
+    Barbara Ramos |	Graphic Designer     |	barbara-ramos-graphic-designer.jpg 
+*/
+
+// MILESTONE 0: Create the array of objects with the provided information.
+// Create object array called data which keep inside arrays with properties firstName, lastName, job and profilImg
